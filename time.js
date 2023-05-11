ae45e5efc1ad5320e9eaccef731be2fee867f6ca
Anugh29 <115694455+Anugh29@users.noreply.github.com>
2023-05-11 21:58:46 +0530

    time.js

--- time.js
+++ time.js
@@ -15,3 +15,33 @@ function updateCountdown()
     countdownEl.innerHTML = `${minutes}:${seconds}`;
     time--;
 }
+
+var movies = [
+    { title: 'PS 2' },
+    { title: 'Kerla Story' },
+    { title: 'Tiger 3'},
+    
+  ];
+  
+  function searchMovies() {
+    var searchTerm = document.getElementById('searchInput').value.toLowerCase();
+    var searchResults = document.getElementById('searchResults');
+    searchResults.innerHTML = '';
+  
+    for (var i = 0; i < movies.length; i++) {
+      var movie = movies[i];
+      var title = movie.title.toLowerCase();
+  
+      if (title.includes(searchTerm)) {
+        var movieElement = document.createElement('div');
+        movieElement.textContent = movie.title;
+        searchResults.appendChild(movieElement);
+      }
+    }
+  
+    if (searchResults.innerHTML === '') {
+      var noResultsElement = document.createElement('p');
+      noResultsElement.textContent = 'No results found.';
+      searchResults.appendChild(noResultsElement);
+    }
+  }
